0b4946574e62869e46f5233a722bc145b8bb74d2
Pratik Jadhav <161144869+pratikjad@users.noreply.github.com>
2026-06-09 11:33:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API = 'http://127.0.0.1:8000';
+const API = 'https://hospital-management-sw4d.onrender.com';
 
 function App() {
   const [username, setUsername] = useState('');
